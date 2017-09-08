ad1387dc7b37d604511b2fd4718764ec2ce62fa5
Johan Janse van Rensburg <johanjvrens@optsci.co.za>
2017-09-08 14:16:31 +0200

    Ignore dist folder for git, added dist paths and src for css, js, html, img

--- README.md
+++ README.md
@@ -31,17 +31,18 @@ Outlining a basic folder structure with three main folders for this project.
 
 **Note**: There is no real need for it but I used `.njk` as extension for all the *src* files used by *nunjucks*.
 
-https://mozilla.github.io/nunjucks/
-https://adonisjs.com/docs/3.2/templating
-https://zellwk.com/blog/nunjucks-with-gulp/
-https://nystudio107.com/blog/a-gulp-workflow-for-frontend-development-automation
-https://nystudio107.com/blog/a-better-package-json-for-the-frontend
-https://medium.com/@andy.neale/nunjucks-a-javascript-template-engine-7731d23eb8cc
-https://css-tricks.com/killer-features-of-nunjucks/
-https://css-tricks.com/component-led-design-patterns-nunjucks-grunt/
-http://alferov.github.io/awesome-gulp/
-https://css-tricks.com/gulp-for-beginners/
-https://hackernoon.com/how-to-automate-all-the-things-with-gulp-b21a3fc96885
+[link](https://mozilla.github.io/nunjucks/)
+[link](https://adonisjs.com/docs/3.2/templating)
+[link](https://zellwk.com/blog/nunjucks-with-gulp/)
+[link](https://nystudio107.com/blog/a-gulp-workflow-for-frontend-development-automation)
+[link](https://nystudio107.com/blog/a-better-package-json-for-the-frontend)
+[link](https://medium.com/@andy.neale/nunjucks-a-javascript-template-engine-7731d23eb8cc)
+[link](https://css-tricks.com/killer-features-of-nunjucks/)
+[link](https://css-tricks.com/component-led-design-patterns-nunjucks-grunt/)
+[link](http://alferov.github.io/awesome-gulp/)
+[link](https://css-tricks.com/gulp-for-beginners/)
+[link](https://hackernoon.com/how-to-automate-all-the-things-with-gulp-b21a3fc96885)
+[link](https://workboxjs.org/get-started/gulp.html)
 
-https://medium.freecodecamp.org/the-anatomy-of-a-bootstrap-dashboard-that-earns-1-000s-each-month-ed3404010d25
-https://medium.com/@programmiri/my-favored-scss-setup-with-bootstrap-4-547e9ea290f8
+[link](https://medium.freecodecamp.org/the-anatomy-of-a-bootstrap-dashboard-that-earns-1-000s-each-month-ed3404010d25)
+[link](https://medium.com/@programmiri/my-favored-scss-setup-with-bootstrap-4-547e9ea290f8)
--- gulpfile.js
+++ gulpfile.js
@@ -30,35 +30,48 @@ const banner = [
 ].join("\n");
 
 // static server + watching scss/njk files
-gulp.task("serve", ["scss", "njk"], function() {
+gulp.task("serve", ["scss"], function() {
   $.browserSync.init({ server: pkg.paths.build.base });
-  gulp.watch(pkg.paths.src.scss, ["scss"]);
-  gulp.watch(pkg.paths.src.templates, ["njk-watch"]);
-  gulp.watch(pkg.paths.src.njk, ["njk-watch"]);
+  // scss file watcher
+  gulp.watch(pkg.paths.src.scss, ["scss"]).on("change", function(event) {
+    $.fancyLog(
+      "File " +
+        event.type +
+        $.chalk.cyanBright(" scss: ") +
+        $.chalk.magentaBright(event.path)
+    );
+  });
+  // njk file watcher
+  gulp
+    .watch([pkg.paths.src.njk, pkg.paths.src.templates], ["njk-watch"])
+    .on("change", function(event) {
+      $.fancyLog(
+        "File " +
+          event.type +
+          $.chalk.cyanBright(" njk: ") +
+          $.chalk.magentaBright(event.path)
+      );
+    });
 });
 
 // scss - build the scss to the build (tmp) folder, including the required paths, and writing out a sourcemap
-gulp.task("scss", () => {
-  $.fancyLog(
-    $.chalk.yellowBright("Compiling: ") + $.chalk.greenBright("scss to css.")
-  );
+gulp.task("scss", ["njk"], function() {
+  $.fancyLog("Compile: " + $.chalk.greenBright("scss to css."));
   return gulp
     .src(pkg.paths.src.scss)
     .pipe($.plumber({ errorHandler: onError }))
     .pipe($.sourcemaps.init({ loadMaps: true }))
     .pipe($.sass().on("error", $.sass.logError))
+    .pipe($.autoprefixer({ browsers: ["last 2 versions", "> 5%"] }))
     .pipe($.cleanCss({ compatibility: "ie9" }))
-    .pipe($.header(banner, { pkg: pkg }))
-    .pipe($.sourcemaps.write("./"))
+    .pipe($.sourcemaps.write("./maps"))
     .pipe(gulp.dest(pkg.paths.build.css))
     .pipe($.browserSync.stream());
 });
 
 // njk - build the nunjucks templates to the build (tmp) folder as html files
-gulp.task("njk", () => {
-  $.fancyLog(
-    $.chalk.yellowBright("Compiling: ") + $.chalk.greenBright("njk to html.")
-  );
+gulp.task("njk", function() {
+  $.fancyLog("Compile: " + $.chalk.greenBright("njk to html."));
   return gulp
     .src(pkg.paths.src.njk)
     .pipe(
@@ -76,8 +89,63 @@ gulp.task("njk-watch", ["njk"], function(done) {
   done();
 });
 
+// js - lint, minify and copy js files to (tmp) folder
+gulp.task("js", function() {
+  $.fancyLog("Process: " + $.chalk.greenBright("js."));
+  return gulp
+    .src(pkg.paths.src.js)
+    .pipe($.plumber({ errorHandler: onError }))
+    .pipe($.sourcemaps.init())
+    .pipe($.uglify())
+    .pipe($.sourcemaps.write("./maps"))
+    .pipe(gulp.dest(pkg.paths.build.js));
+});
+
+// Strip, prefix, minify and concatenate CSS during a deployment
+gulp.task("css-dist", ["scss"], function() {
+  $.fancyLog($.chalk.greenBright("• run CSS tasks for distribution"));
+  return gulp
+    .src(pkg.paths.src.css)
+    .pipe($.plumber({ errorHandler: onError }))
+    .pipe($.header(banner, { pkg: pkg }))
+    .pipe(gulp.dest(pkg.paths.dist.css));
+});
+
+// Strip, minify and concatenate your JavaScript during a deployment
+gulp.task("js-dist", ["js"], function() {
+  $.fancyLog($.chalk.greenBright("• run JavaScript tasks for distribution"));
+  return gulp
+    .src(pkg.paths.src.js)
+    .pipe($.plumber({ errorHandler: onError }))
+    .pipe($.uglify())
+    .pipe(gulp.dest(pkg.paths.dist.js));
+});
+
+// html-dist - build the nunjucks templates to the build (tmp) folder as html files
+gulp.task("html-dist", ["njk"], function() {
+  $.fancyLog($.chalk.greenBright("• run HTML tasks for distribution"));
+  return gulp.src(pkg.paths.src.html).pipe(gulp.dest(pkg.paths.dist.html));
+});
+
+// service worker
+gulp.task("bundle-sw", ["serve"], function() {
+  return $.workboxBuild
+    .generateSW({
+      globDirectory: pkg.paths.build.base,
+      swDest: pkg.paths.build.base + "sw.js",
+      globPatterns: ["**/*.{html,js,css}"],
+      globIgnores: ["admin.html"]
+    })
+    .then(() => {
+      console.log("Service worker generated.");
+    })
+    .catch(err => {
+      console.log("[ERROR] This happened: " + err);
+    });
+});
+
 // default serve (runs on yarn start)
 gulp.task("default", ["serve"]);
 
 // production build (runs on yarn build)
-gulp.task("build", ["scss", "njk"]);
+gulp.task("build", ["bundle-sw", "css-dist", "js-dist", "html-dist"]);
